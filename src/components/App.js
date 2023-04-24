2b03092a5e29434e4d5eb426010ced1d170ae125
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-04-24 14:30:08 -0700

    show map, add markers, import GeoJSO data, WIP: Setup neighborhood boundaries

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,53 @@
-import logo from '../logo.svg'
 import '../App.css';
+import { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker} from 'google-maps-react';
+import geoJSONData from '../data/geoJSONData.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MapContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      markers: []
+    };
+  }
+
+  onMapClick = (mapProps, map, clickEvent) => {
+    const newMarker = {
+      lat: clickEvent.latLng.lat(),
+      lng: clickEvent.latLng.lng()
+    };
+    this.setState({ markers: [...this.state.markers, newMarker] });
+  };
+
+  render() {
+    const { google } = this.props;
+    const { markers } = this.state;
+
+    return (
+      <Map
+        google = {google}
+        style={{ width:"100%", height:"100%" }}
+        zoom = {10}
+        initialCenter= {{ lat: 40, lng: -80 }}
+        mapContainerClassName="map-container"
+        onClick={this.onMapClick}
+      >
+        <geoJSON
+          data={geoJSONData}
+          fillColor="#FF0000"
+          strokeColor="#000000"
+        />
+      {markers.map((marker, index) => (
+         <Marker 
+         key={index}
+         position={marker} 
+         />
+      ))}
+      </Map>
+    );
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_GMAP_KEY
+})(MapContainer)
