dbf1552bff6497dc3900ed106a152154ba78a663
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-04-28 16:54:48 -0700

    update splashscreen background, include instructions

--- src/components/Map.js
+++ src/components/Map.js
@@ -124,7 +124,7 @@ class MapContainer extends Component {
           <img src="../src/Logos/svg/logo-black.svg" alt="map logo" />
 
           <h1>Welcome to the AI Map Concierge!</h1>
-          <h3>This app allows you to get AI generated information about any location on the map.</h3>
+          <h3>This app allows you to AI generate\ information about any location on the map.</h3>
           <div className='splash-p-tags'>
             <p> 1. Click on the map to add a marker</p>
             <p> 2. Click on the marker to open the sidebar</p>
@@ -133,6 +133,8 @@ class MapContainer extends Component {
             <br />
             <p> - You can also enter your own question and see what it has to say</p>
             <p> - Mess around with the advanced settings to see what kind of results you can get!</p>
+            <br />
+            <p> <em>* Results may be inaccurate, offensive, or harmful. </em></p>
             </div>
         </div>
       );
