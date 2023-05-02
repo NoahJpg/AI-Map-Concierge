6c55db388c3ad43b9aa53e9ed31bc5cfa8bb1f5f
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-05-02 14:37:52 -0700

    update firebase.js to hold config object

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,16 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 
-const apiKey = process.env.REACT_APP_FIREBASE_API_KEY;
-const authDomain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
-const projectId = process.env.REACT_APP_FIREBASE_PROJECT_ID;
-const storageBucket = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
-const messagingSenderId = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID;
-const appId = process.env.REACT_APP_FIREBASE_APP_ID;
-const measurementId = process.env.REACT_APP_FIREBASE_MEASUREMENT_ID;
+const firebaseConfig = {
+apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+appId: process.env.REACT_APP_FIREBASE_APP_ID,
+measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+}
+
 
 
 const app = initializeApp(firebaseConfig);
